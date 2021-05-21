f034fe80bfe9fc5408ee00ae823d4d434954ab00
midsdsy <369708449@qq.com>
2021-05-21 19:30:19 +0800

    update .gitignore

--- docs/modules/export.rst
+++
@@ -1,9 +0,0 @@
-detectron2.export package
-=========================
-
-Related tutorial: :doc:`../tutorials/deployment`.
-
-.. automodule:: detectron2.export
-    :members:
-    :undoc-members:
-    :show-inheritance:
--- projects/LOD-Net/lod_net/__init__.py
+++
@@ -1,6 +0,0 @@
-from .lod_roi_head import LODROIHeads
-from .lod_mask_head import  LODMaskRCNNConvUpsampleHead
-from .lod_head import LODBorderHead
-from .config import add_lod_config
-from .motionblur_augmentation import MotionBlur
-from .motionblur_transform import MotionBlurTransform
