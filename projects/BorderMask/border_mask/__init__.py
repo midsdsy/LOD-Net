da5263fc8fc5c82d8eba95147aa8c51f75c1d08b
midsdsy <369708449@qq.com>
2021-05-21 23:48:35 +0800

    update .gitignore

--- projects/BorderMask/border_mask/__init__.py
+++
@@ -1,7 +0,0 @@
-from .border_mask_roi_head import BorderMaskROIHeads
-from .mask_head import  BorderMaskRCNNConvUpsampleHead
-from .border_head import BorderHead
-from .parallel_border_head import ParallelBorderHead
-from .config import add_bordermask_config
-from .motionblur_augmentation import MotionBlur
-from .motionblur_transform import MotionBlurTransform
